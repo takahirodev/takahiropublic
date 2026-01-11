51b83cc58aad708314d04b45d2753bbd4f042da1
takahiro <takahirorobloxdev@gmail.com>
2026-01-11 17:05:57 -0500

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,4 @@
+local placeid = game.PlaceId
+if placeid == 118614517739521 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/takahiroxdev/takahiro/main/games/118614517739521.lua"))()
+end
