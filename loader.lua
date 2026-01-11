4f3c69ab4e9d6bf40fb3f490da21e01715d1012e
takahiro <takahirorobloxdev@gmail.com>
2026-01-11 17:07:00 -0500

    Delete loader.lua

--- loader.lua
+++
@@ -1,4 +0,0 @@
-local placeid = game.PlaceId
-if placeid == 118614517739521 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/takahiroxdev/takahiro/main/games/118614517739521.lua"))()
-end
